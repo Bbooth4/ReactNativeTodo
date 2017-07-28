649362efc59a574d67b78d5aad9fe0e8bbd2d1de
Brandon <booth.brandon4@gmail.com>
2017-07-28 12:52:09 -0400

    started app

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+     this.state = { 
+       todos: [
+         { task: 'Learn React Native' }
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// AppRegistry.registerComponent('main'm () => App);
